f9617c9070a47fd97dcc605d638237cb7de55151
tahirshaikh85 <tahirshaikhrs7@gmail.com>
2023-12-22 11:26:05 +0530

    product endpoints

--- controller/controller.js
+++ controller/controller.js
@@ -441,3 +441,62 @@ exports.trendingProducts = async (req, res) => {
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+// -----------------------------------------------------
+
+exports.fetchAllProducts = async (req, res) => {
+    
+  let query = productsDB.find({}).sort({ createdAt: -1 });
+
+  if (req.query.category) {
+      query = query.find({ category: req.query.category });
+  }
+  if (req.query.label) {
+      query = query.find({ label: req.query.label });
+  }
+  if (req.query._sort && req.query._order) {
+      query = query.sort({ [req.query._sort]: req.query._order });
+  }
+
+  try {
+      const docs = await query.exec();
+      res.status(200).json(docs);
+  } catch (err) {
+      res.status(400).json(err);
+  }
+};
+
+// fetch particular product
+exports.fetchProductById = async (req, res) => {
+  try {
+      let { id } = req.query;
+      const product = await productsDB.findOne({ _id: id });
+      res.status(201).send(product);
+
+  } catch (error) {
+      res.status(500).send(error);
+  }
+}
+
+// delete particular product by ID
+exports.deleteProductById = async (req, res) => {
+  try {
+      const { id } = req.query;
+
+      // Check if the provided ID is valid
+      if (!id) {
+          return res.status(400).json({ error: 'Product ID is missing.' });
+      }
+
+      const deletedProduct = await productsDB.findByIdAndDelete({ _id: id });
+
+      if (!deletedProduct) {
+          return res.status(404).json({ error: 'Product not found.' });
+      }
+
+      res.status(200).json({ message: 'Product deleted successfully.' });
+  } catch (error) {
+      console.error('Error while deleting product:', error);
+      res.status(500).json({ error: 'An error occurred while deleting the product.' });
+  }
+};
--- routes/routes.js
+++ routes/routes.js
@@ -1,45 +1,54 @@
 const router = require("express").Router();
 const controller = require("../controller/controller");
 
-router.get('/categories',controller.getCategories);
-router.get('/citiesAndHobbies',controller.getCitiesAndHobbies);
+// categories of products
+router.get("/categories", controller.getCategories);
+router.get("/citiesAndHobbies", controller.getCitiesAndHobbies);
 
-router.post('/preferences',controller.preferences);
-router.post('/createProduct',controller.createProduct);
-
-router.get('/getRecommendation',controller.getRecommendation);
+// recommendation engine
+router
+  .post("/preferences", controller.preferences)
+  .get("/getRecommendation", controller.getRecommendation);
 
 // ✔❌ not necessary - only for development purpose (one Time Use)
 // router.post('/saveCategories',controller.saveCategories);
 // router.post('/saveCitiesAndHobbies',controller.saveCitiesAndHobbies);
 
-
 // ---------------------------------------
 
-router.post('/checkout', controller.checkout);
-router.post('/order', controller.order);
+router.post("/checkout", controller.checkout);
+router.post("/order", controller.order);
 
-router.post('/cart',controller.createCart);
-router.get('/cart',controller.getCart);
+// cart
+router.post("/cart", controller.createCart).get("/cart", controller.getCart);
 
 // home
-router.post('/createImgCricle',controller.createImgCircle);
-router.get('/getImgCircle',controller.getSliderCircle);
+router
+  .post("/createImgCricle", controller.createImgCircle)
+  .get("/getImgCircle", controller.getSliderCircle);
 
-router.post('/createMidInfoSection',controller.createMidInfoSection);
-router.get('/getMidInfoSection',controller.getMidInfoSection);
+router
+  .post("/createMidInfoSection", controller.createMidInfoSection)
+  .get("/getMidInfoSection", controller.getMidInfoSection);
 
-router.post('/createHeaderInfoSection',controller.createHeaderInfoSection);
-router.get('/getHeaderInfoSection',controller.getHeaderInfoSection);
+router
+  .post("/createHeaderInfoSection", controller.createHeaderInfoSection)
+  .get("/getHeaderInfoSection", controller.getHeaderInfoSection);
 
-router.post('/createImgSection',controller.createImgSection);
-router.get('/getImgSection',controller.getImgSection);
+router
+  .post("/createImgSection", controller.createImgSection)
+  .get("/getImgSection", controller.getImgSection);
 
-// increment the popularity of a product
-router.post('/increment-popularity',controller.incrementPopularity);
+// trending products
+router
+  .post("/increment-popularity", controller.incrementPopularity) // increment the popularity of a product
+  .get("/trending-products", controller.trendingProducts); // fetch trending products
 
-// fetch trending products
-router.get('/trending-products',controller.trendingProducts)
+// ---------------- product endpoints
+router
+  .post("/createProduct", controller.createProduct)
+  .get("/products", controller.fetchAllProducts)
+  .get("/getSingleProduct", controller.fetchProductById)
+  .delete("/products", controller.deleteProductById);
 
-
-module.exports = router
+module.exports = router;
