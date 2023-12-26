a148f0e0b557c4c99345e47ccfb27e4db0d5e924
unknown <tahirshaikhrs7@gmail.com>
2023-12-26 17:32:57 +0530

    update user profile

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -8,7 +8,7 @@ const verifyToken = (req, res, next) => {
     return res.status(403).json({ message: 'Token not provided' });
   }
 
-  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+  jwt.verify(token.replace('Bearer ', ''), process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(401).json({ message: 'Failed to authenticate token' });
     }
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const passport = require("passport");
 const router = require("express").Router();
 const verifyToken = require("../middleware/verifyToken");
-const generateToken = require("../config/jwt")
+const generateToken = require("../config/jwt");
+const userDB = require("../model/User")
 
 // initial google ouath login
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
@@ -16,14 +17,50 @@ router.get('/google/callback',
   });
 
 
-// Endpoint to check user authentication status
-router.get('/user', (req, res) => {
-  if (req.isAuthenticated()) {
-    // User is authenticated, send user information
-    res.json({ isAuthenticated: true, user: req.user });
-  } else {
-    // User is not authenticated
-    res.json({ isAuthenticated: false, user: null });
+// to check user authentication status
+router.get('/user', verifyToken, async (req, res) => {
+  try {
+    if (req.isAuthenticated()) {
+      // User is authenticated, send user information
+      const userInfo = await userDB.find({ googleId: req.user.id });
+
+      if (userInfo.length > 0) {
+        const userObject = userInfo[0]; 
+        res.json({ isAuthenticated: true, user: userObject });
+      } else {
+        res.json({ isAuthenticated: false, user: null });
+      }
+    } else {
+      // User is not authenticated
+      res.json({ isAuthenticated: false, user: null });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+
+// Update user profile
+router.put('/update-profile', verifyToken, async (req, res) => {
+  try {
+    const googleId = req.user.id;
+    const {
+      displayName,
+      email,
+      photoURL
+    } = req.body;
+
+
+    const updatedUser = await userDB.findOneAndUpdate(
+      { googleId },
+      { displayName,email,photoURL },
+      { new: true }
+    );
+
+    res.json({ message: 'Profile updated successfully', user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
