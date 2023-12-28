9a1b5fa5178cf289b70515db64ec1cb64a72d67d
tahirshaikh85 <tahirshaikhrs7@gmail.com>
2023-12-28 09:35:15 +0530

    update product schema

--- model/Products.js
+++ model/Products.js
@@ -2,16 +2,68 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   productId: { type: String, required: true, unique: true },
-  productName: { type: String, required: true },
+  patternNumber: { type: String, required: true, unique: true },
+  productTitle: { type: String, required: true },
+  roomCategory: { type: String, required: true },
   category: { type: String, required: true },
   subcategory: { type: String, required: true },
-  description: {type: String},
+  collectionName: { type: String},
   images: { type: [String] },
-  price: { type: String },
-  ratings: { type: Number }, 
-  reviews: { type: [String] }, 
-  tags: { type: [String] },
-  colors:{type:[String]},
+  perUnitPrice: { type: Number },
+  ratings: [{
+    userId: { type: String, required: true },
+    rating: { type: Number, required: true },
+    review: { type: String },
+    timestamp: { type: Date, default: Date.now },
+  }],
+  colors: { type: [String] },
+  dimensions: {
+    length: {
+      value: {
+        type: Number,
+        required: true,
+      },
+      unit: {
+        type: String,
+        enum: ['mm', 'cm', 'm', 'in', 'ft'],
+        required: true,
+      },
+    },
+    width: {
+      value: {
+        type: Number,
+        required: true,
+      },
+      unit: {
+        type: String,
+        enum: ['mm', 'cm', 'm', 'in', 'ft'],
+        required: true,
+      },
+    },
+    thickness: {
+      value: {
+        type: Number,
+        required: true,
+      },
+      unit: {
+        type: String,
+        enum: ['mm', 'cm', 'm', 'in', 'ft'],
+        required: true,
+      },
+    },
+  },
+  units: Number,
+  unitType: {
+    type: String,
+    enum: ['sqft', 'box', 'pcs', 'mtr'],
+    required: true,
+  },
+  perUnitType: {
+    type: String,
+    enum: ['sqft', 'box', 'pcs', 'mtr'],
+    required: true,
+  },
+  totalPrice: Number,
   popularity: {
     type: Number,
     default: 0,
